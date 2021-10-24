1df87725025e66ab45bdcfb0dec762d0284cbeed
Cavel Peck <cavelpeck@gmail.com>
2021-10-24 17:51:45 -0500

    Edited the style.css file and sytles.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 /* Add your JavaScript to this file */
+windows.onload = funtion() 
+{
+
+  let btn = document.getElementsByClassName("button")[0];
+  btn.addEventListener("click", function(e){
+     e.preventDefault();
+  var email = document.getElementById("email");
+  var message = document.getElementsByClassName("")    
+
+     }
+ )}
