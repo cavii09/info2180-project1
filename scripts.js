16198e961b2d06858765b11a865dec40fd53d4ec
Cavel Peck <cavelpeck@gmail.com>
2021-10-24 21:07:17 -0500

    Updated the style.css file and sytles.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
 /* Add your JavaScript to this file */
 windows.onload = funtion() 
 {
-
   let btn = document.getElementsByClassName("button")[0];
   btn.addEventListener("click", function(e){
      e.preventDefault();
   var email = document.getElementById("email");
-  var message = document.getElementsByClassName("")    
+  var message = document.getElementsByClassName("message");
+  
+  var email =newemail.value;
 
-     }
+  if (email=='email'){
+      alert("Thank you! Your email address ${newemail} had been added to our mailing list!")
+      return false;
+    }    
+  else
+  {
+      alert("Please enter a valid email address.")
+  }
+
+
+     } 
  )}
